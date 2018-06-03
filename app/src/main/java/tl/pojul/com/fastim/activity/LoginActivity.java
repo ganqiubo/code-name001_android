5f009485d09f0ce8e8768393e6cfa697a1e7ef45
ganqiubo <2443974192@qq.com>
2018-06-03 22:40:41 +0800

    login ui modify

--- app/src/main/java/tl/pojul/com/fastim/activity/LoginActivity.java
+++ app/src/main/java/tl/pojul/com/fastim/activity/LoginActivity.java
@@ -2,15 +2,13 @@ package tl.pojul.com.fastim.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-
-import tl.pojul.com.fastim.MyApplication;
-import tl.pojul.com.fastim.R;
-import tl.pojul.com.fastim.util.Constant;
-import tl.pojul.com.fastim.util.DialogUtil;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.pojul.fastIM.message.request.LoginMessage;
 import com.pojul.fastIM.message.response.LoginResponse;
@@ -18,24 +16,46 @@ import com.pojul.objectsocket.message.ResponseMessage;
 import com.pojul.objectsocket.socket.ClientSocket;
 import com.pojul.objectsocket.socket.SocketRequest;
 
-public class LoginActivity extends BaseActivity implements View.OnClickListener{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import tl.pojul.com.fastim.MyApplication;
+import tl.pojul.com.fastim.R;
+import tl.pojul.com.fastim.util.Constant;
+import tl.pojul.com.fastim.util.DialogUtil;
 
-    private Button loginSubmit;
-    private EditText loginAccount;
-    private EditText loginPasswd;
+public class LoginActivity extends BaseActivity{
+
+
+    @BindView(R.id.login_account)
+    EditText loginAccount;
+    @BindView(R.id.login_passwd)
+    EditText loginPasswd;
+    @BindView(R.id.login_passwd_visiable)
+    CheckBox loginPasswdVisiable;
+    @BindView(R.id.login_autologin)
+    CheckBox loginAutologin;
+    @BindView(R.id.login_forgetPasswd)
+    TextView loginForgetPasswd;
+    @BindView(R.id.login_submit)
+    Button loginSubmit;
+    @BindView(R.id.login_register)
+    TextView loginRegister;
+    @BindView(R.id.activity_login)
+    RelativeLayout activityLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
-        loginAccount = findViewById(R.id.activity_login_account);
+        /*loginAccount = findViewById(R.id.activity_login_account);
         loginPasswd = findViewById(R.id.activity_login_passwd);
-        loginSubmit = findViewById(R.id.activity_login_submit);
-        loginSubmit.setOnClickListener(this);
+        loginSubmit = findViewById(R.id.activity_login_submit);*/
     }
 
-    private void conn(){
+    private void conn() {
         DialogUtil.getInstance().showLoadingDialog(this, "连接服务器中...");
         new SocketRequest().resuestConn(new SocketRequest.IRequestConn() {
             @Override
@@ -50,7 +70,7 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
             public void onFinished(ClientSocket clientSocket) {
                 runOnUiThread(() -> {
                     DialogUtil.getInstance().dimissLoadingDialog();
-                    if(clientSocket == null || clientSocket.getmSocket() == null){
+                    if (clientSocket == null || clientSocket.getmSocket() == null) {
                         showLongToas("网络连接失败");
                         return;
                     }
@@ -61,13 +81,13 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
         }, Constant.HOST, Constant.PORT);
     }
 
-    private void login(){
+    private void login() {
         DialogUtil.getInstance().showLoadingDialog(this, "登陆中...");
         LoginMessage mLoginMessage = new LoginMessage();
         mLoginMessage.setUserName(loginAccount.getText().toString());
         mLoginMessage.setPassWd(loginPasswd.getText().toString());
         mLoginMessage.setDeviceType("Android");
-        new SocketRequest().resuest(MyApplication.ClientSocket, mLoginMessage, new SocketRequest.IRequest(){
+        new SocketRequest().resuest(MyApplication.ClientSocket, mLoginMessage, new SocketRequest.IRequest() {
             @Override
             public void onError(String msg) {
                 runOnUiThread(() -> {
@@ -75,17 +95,14 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
                     showLongToas("网络连接失败");
                 });
             }
+
             @Override
             public void onFinished(ResponseMessage mResponse) {
                 runOnUiThread(() -> {
                     DialogUtil.getInstance().dimissLoadingDialog();
-                    /*if(MyApplication.ClientSocket == null || MyApplication.ClientSocket.getmSocket() == null){
-                        showLongToas("与服务器连接已断开");
-                        return;
-                    }*/
-                    LoginResponse loginResponse = (LoginResponse)mResponse;
+                    LoginResponse loginResponse = (LoginResponse) mResponse;
                     showLongToas(mResponse.getMessage());
-                    if(loginResponse.getCode() == 200) {
+                    if (loginResponse.getCode() == 200) {
                         startActivity(new Intent(LoginActivity.this, MainActivity.class));
                         finish();
                     }
@@ -94,11 +111,26 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
         });
     }
 
-    @Override
+    @OnClick({R.id.login_submit, R.id.login_passwd_visiable})
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.activity_login_submit:
+        switch (view.getId()) {
+            case R.id.login_submit:
+                if ("".equals(loginAccount.getText().toString())) {
+                    showShortToas("用户名不能为空");
+                    return;
+                }
+                if ("".equals(loginPasswd.getText().toString())) {
+                    showShortToas("密码不能为空");
+                    return;
+                }
                 conn();
+                break;
+            case R.id.login_passwd_visiable:
+                if(loginPasswdVisiable.isChecked()){
+                    loginPasswd.setInputType(InputType.TYPE_CLASS_TEXT);
+                }else{
+                    loginPasswd.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
                 break;
         }
     }
